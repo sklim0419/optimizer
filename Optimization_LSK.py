b7fbfaaceb05f70da1e7499806f5ccca00aa4ff9
sklim0419 <lsk041958@gmail.com>
2025-05-13 13:53:41 +0900

    numerical descent

--- Optimization_LSK.py
+++ Optimization_LSK.py
@@ -6,7 +6,7 @@ import sample_data as data
 
 if __name__ == "__main__":
     samples, true_pi = data.generate_sample_data()
-    est_pi = gd.estimate_inertial_parameters(samples, learning_rate=1e-4, max_iter=1000,numerical=False)
+    est_pi = gd.estimate_inertial_parameters(samples, learning_rate=1e-4, max_iter=1000,numerical=True)
     print("\n[True Inertial Parameters]\n", true_pi)
     print("\n[Estimated Inertial Parameters]\n", est_pi)
     print("\n[Relative Error (%)]\n", 100 * np.abs((est_pi - true_pi) / true_pi))
